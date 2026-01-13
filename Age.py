277e779075794f8298e9a032c4c5ff19aed1f9b5
giandoctor <gian.doctor@hotmail.com>
2026-01-13 12:11:55 +1100

    Age

---
+++ Age.py
@@ -0,0 +1,6 @@
+#For some reason the user input doesn't work for me.
+age=11
+if age >= 10 and age <= 20:
+        print("Your age is between 10 and 20 .")
+else:
+    print("Your age is outside the range.")
