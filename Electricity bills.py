11a6ef6d4fee4235b79d03036eb8d20a35be6378
giandoctor <gian.doctor@hotmail.com>
2026-01-13 12:00:04 +1100

    Electricity bill

---
+++ Electricity bills.py
@@ -0,0 +1,15 @@
+unit=int(input("Enter the number of units you consumed"))
+if(unit<50):
+    amount=unit*2.60
+    surcharge=25
+elif(unit<100):
+    amount=130+((unit-50)*3.25)
+    surcharge=35
+elif(unit<=200):
+    amount=130+160.50+((unit-100)*5.26)
+    surcharge=45
+else:
+    amount=130+160.50+526+((unit-200)*8.45)
+    surcharge=75
+total=amount+surcharge
+print("/nElectricity bill=%.2f"%total)
